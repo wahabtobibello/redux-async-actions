9e9dcb61f378e0b9d7ba613fbef104f1acb2952a
Wahab Bello <wahaaabello@gmail.com>
2020-03-24 22:09:26 +0100

    feature: add async reducer

--- src/index.ts
+++ src/index.ts
@@ -83,3 +83,5 @@ export const createAsyncSaga = (
       }
     }
   };
+
+export * from './reducer';
---
+++ src/reducer.ts
@@ -0,0 +1,65 @@
+import { combineReducers } from 'redux';
+import { Action } from 'redux-actions';
+import _ from 'lodash';
+
+export const loadingReducer = (state = {}, action: Action<{}>) => {
+  const { type } = action;
+  const matches = /(.*)_(REQUEST|SUCCESS|FAILURE)/.exec(type);
+  if (!matches) return state;
+
+  const [, requestName, requestState] = matches;
+  return {
+    ...state,
+    [requestName]: requestState === 'REQUEST'
+  };
+};
+
+export const errorReducer = (state = {}, action: Action<{}>) => {
+  const { type, error } = action;
+  const matches = /(.*)_(REQUEST|FAILURE)/.exec(type);
+
+  if (!matches) return state;
+
+  const [, requestName, requestState] = matches;
+  return {
+    ...state,
+    [requestName]: requestState === 'FAILURE' ? error : null
+  };
+};
+
+export const createLoadingSelector = (requestNames: any[]) => (state: any) =>
+  _(requestNames)
+    .castArray()
+    .some(type =>
+      _.get(
+        state,
+        `async.loading.${type.replace(/_(REQUEST|SUCCESS|FAILURE)/, '')}`
+      )
+    );
+
+export const createErrorSelector = (requestNames: any[]) => (state: any) =>
+  _(requestNames)
+    .castArray()
+    .map(type =>
+      _.get(
+        state,
+        `async.errors.${type.replace(/_(REQUEST|SUCCESS|FAILURE)/, '')}`
+      )
+    )
+    .compact()
+    .first() || null;
+
+export const createAsyncSelector = (requestNames: any[]) => {
+  const getLoading = createLoadingSelector(requestNames);
+  const getError = createErrorSelector(requestNames);
+
+  return (state: any) => ({
+    loading: getLoading(state),
+    error: getError(state)
+  });
+};
+
+export const asyncReducer = combineReducers({
+  loading: loadingReducer,
+  errors: errorReducer
+});
